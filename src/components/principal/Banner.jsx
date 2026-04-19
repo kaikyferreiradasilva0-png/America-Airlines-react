293bb472d8f45bead6396eb10fa988b0d07104eb
kaikyferreiradasilva0 <kaikyferreiradasilva0@gmail.com>
2026-04-19 19:57:24 -0300

    primeiro commit

---
+++ src/components/principal/Banner.jsx
@@ -0,0 +1,11 @@
+import './Banner.css'
+function Banner() {
+    return (
+        <section id="home" className="banner">
+            <h2>Soluções Inovadoras em TI</h2>
+            <p>Transformando ideias em arquiteturas escaláveis e interfaces modernas.</p>
+            <button className="cta-btn">Saiba Mais</button>
+        </section>
+    )
+}
+export default Banner;
